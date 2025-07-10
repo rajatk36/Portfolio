812bdb63916a38d72d562d0ecbb68cb87105ed16
rajatk36 <134360910+rajatk36@users.noreply.github.com>
2025-07-10 20:27:21 +0530

    Update Home.jsx

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -4,9 +4,9 @@ import './Home.css';
 
 const Home = () => {
   
-   const lnkedin_link = "https://www.linkedin.com/in/rajat-kumar-76b020261/";
-  const instagram_link = "https://www.instagram.com/rajatkumar_007/";
-  const github_link = "https://github.com/rajatk36";
+   const lnkedin_link = `${import.meta.env.VITE_APP_LINKEDIN}`;
+  const instagram_link = `${import.meta.env.VITE_APP_INSTAGRAM}`;
+  const github_link = `${import.meta.env.VITE_APP_GITHUB}`;
   
   return (
       <div className="home">
