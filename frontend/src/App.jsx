5ab533c6585c1dad5d3fa31f3cb0fba9e8b18338
rajatk36 <134360910+rajatk36@users.noreply.github.com>
2025-07-10 06:47:14 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import './App.css';
 import Navbar from './components/Navbar/Navbar.jsx';
 import Home from './components/Home/Home.jsx';
 import AboutMe from './components/AboutMe/AboutMe.jsx';
-
+import MyProjects from './components/MyProjects/MyProjects.jsx';
 import Contact from './components/Contact/Contact.jsx';
 import SpiderWeb from './components/SpiderWeb/SpiderWeb.jsx';
 
@@ -18,7 +18,9 @@ function App() {
         <AboutMe />
       </section>
 
-      
+      <section id="projects" className="section-with-background">
+        <MyProjects />
+      </section>
 
       <section id="contact"><Contact /></section>
     </div>
