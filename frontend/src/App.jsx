e07a3d2262a2d167c4147af45d7b84bf9c2f1e6b
rajatk36 <134360910+rajatk36@users.noreply.github.com>
2025-07-11 07:26:31 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar/Navbar.jsx';
-
 import AboutMe from './components/AboutMe/AboutMe.jsx';
 import MyProjects from './components/Myprojects/MyProjects.jsx';
 import Contact from './components/Contact/Contact.jsx';
@@ -11,17 +10,13 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-
       <section id="home"><SpiderWeb /></section>
       <section id="about" className="section-with-background">
-       
         <AboutMe />
       </section>
-
       <section id="projects" className="section-with-background">
         <MyProjects />
       </section>
-
       <section id="contact"><Contact /></section>
     </div>
   );
