383bb03e9ddc9976777de72860605fb45e4f1b0c
rajatk36 <134360910+rajatk36@users.noreply.github.com>
2025-07-11 07:21:51 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar/Navbar.jsx';
-import Home from './components/Home/Home.jsx';
+
 import AboutMe from './components/AboutMe/AboutMe.jsx';
 import MyProjects from './components/Myprojects/MyProjects.jsx';
 import Contact from './components/Contact/Contact.jsx';
