d2354042003c55a38c9b20f5f4ac3391686adf0a
rajatk36 <134360910+rajatk36@users.noreply.github.com>
2025-08-13 08:01:23 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { MdEmail } from "react-icons/md";
-import "./navbar.css";
+import "./Navbar.css";
 
 const Menu = [
   { name: "Home", id: "home" },
@@ -10,53 +10,17 @@ const Menu = [
 ];
 
 const Navbar = () => {
-  const [isMobile, setIsMobile] = useState(window.innerWidth <= 374);
-
-  useEffect(() => {
-    const handleResize = () => {
-      setIsMobile(window.innerWidth <= 374);
-    };
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-
-  const [isVisible, setIsVisible] = useState(true);
-  const [lastScrollY, setLastScrollY] = useState(0);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY < lastScrollY) {
-        setIsVisible(true);
-      } else {
-        setIsVisible(false);
-      }
-      setLastScrollY(window.scrollY);
-    };
-
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, [lastScrollY]);
-  
   const scrollToSection = (id) => {
     const section = document.getElementById(id);
     if (section) {
       section.scrollIntoView({ behavior: "smooth" });
     }
   };
-const resume_link = `${import.meta.env.VITE_RESUME_LINK}`;
-const GoToLink =() => {
-  window.open(resume_link,'_blank');
-};
 
-const responsiveMenu = ismobile
-  ? Menu.filter((item)=> item.name !== "About")
-  : Menu;
-
-  
   return (
-<header className={`header ${isVisible ? "show" : "hide"}`}>
+    <header className="header">
       <nav className="navbar">
-        {responsiveMenu.map((item, index) => (
+        {Menu.map((item, index) => (
           <button
             key={index}
             className="nav-button"
@@ -66,21 +30,14 @@ const responsiveMenu = ismobile
           </button>
           
         ))}
-       <button className="nav-button" onClick={goToLink}>
-        Resume
-        </button> 
+        
       </nav>
-       
-       
-      
-      <button
+       <button
       className="nav-button email-icon"
       onClick={()=>scrollToSection("contact")}>
       <MdEmail size={20} />
       Hire Me
     </button>
-     
-    
     </header>
   );
 };
