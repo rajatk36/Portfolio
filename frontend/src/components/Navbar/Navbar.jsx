fd4b9f8a0bc473b3f8edae6c5ebc8c28727ca2a4
rajatk36 <134360910+rajatk36@users.noreply.github.com>
2025-08-13 07:58:19 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,5 +1,4 @@
-import React from "react";
-import {useState,useEffect} from "react"
+import React, { useState, useEffect } from "react";
 import { MdEmail } from "react-icons/md";
 import "./navbar.css";
 
